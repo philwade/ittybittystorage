d1175ca135f1eb68e68eacde5ea066f84a3a4c2e
Phil Wade <phil@whoisstudio.com>
2017-06-08 16:59:56 -0400

    Add update method

--- ittybitty/web/controllers/bit_controller.ex
+++ ittybitty/web/controllers/bit_controller.ex
@@ -10,4 +10,14 @@ defmodule Ittybitty.BitController do
             |> render(Ittybitty.ErrorView, "404.json")
     end
   end
+
+  def update(conn, %{"id" => key, "value" => value}) do
+    case Ittybitty.Redix.update_key(key, value) do
+      {:notfound} ->
+        put_status(conn, 404)
+        |> render(Ittybitty.ErrorView, "404.json")
+      {:ok, val} ->
+        json conn, %{value: val}
+    end
+  end
 end
