9b5f2ec2a6d60923b6d214a4a73839d645052cc4
Phil Wade <phil@whoisstudio.com>
2017-07-18 09:59:30 -0400

    👏UNIT 👏 TESTS 👏 BAB👏Y

--- ittybitty/lib/ittybitty/store.ex
+++ ittybitty/lib/ittybitty/store.ex
@@ -2,8 +2,8 @@ defmodule Ittybitty.Store do
   @setbit 0
   @valuebit 1
 
-  def get_key(key) do
-    case Redix.pipeline(:redix, [["GETBIT", key, @setbit], ["GETBIT", key, @valuebit]]) do
+  def get_key(key, redis \\ Redix) do
+    case redis.pipeline(:redix, [["GETBIT", key, @setbit], ["GETBIT", key, @valuebit]]) do
       {:ok, [0, _]} ->
         {:notfound}
       {:ok, [1, val]} ->
@@ -13,19 +13,19 @@ defmodule Ittybitty.Store do
     end
   end
 
-  def update_key(key, value) do
-    case Redix.command(:redix, ["GETBIT", key, @setbit]) do
-      {:ok, 1} -> update_key_value(key, value)
+  def update_key(key, value, redis \\ Redix) do
+    case redis.command(:redix, ["GETBIT", key, @setbit]) do
+      {:ok, 1} -> update_key_value(key, value, redis)
       {:ok, 0} -> {:notfound}
       {:error, msg} -> {:error, msg}
     end
   end
 
-  defp update_key_value(key, value) do
-    Redix.command(:redix, ["SETBIT", key, @valuebit, value])
+  defp update_key_value(key, value, redis) do
+    redis.command(:redix, ["SETBIT", key, @valuebit, value])
   end
 
-  def create_key(key) do
-    Redix.pipeline(:redix, [["SETBIT", key, @setbit, 1], ["SETBIT", key, @valuebit, 1]])
+  def create_key(key, redis \\ Redix) do
+    redis.pipeline(:redix, [["SETBIT", key, @setbit, 1], ["SETBIT", key, @valuebit, 1]])
   end
 end
---
+++ ittybitty/test/lib/store_test.exs
@@ -0,0 +1,58 @@
+defmodule Ittybitty.StoreTest do
+  use ExUnit.Case, async: true
+
+  defmodule RedisMock do
+    def pipeline(_, [["GETBIT", "missing", 0], ["GETBIT", "missing", 1]]) do
+      {:ok, [0, 0]}
+    end
+
+    def pipeline(_, [["GETBIT", "found", 0], ["GETBIT", "found", 1]]) do
+      {:ok, [1, 0]}
+    end
+
+    def pipeline(_, [["GETBIT", "error", 0], ["GETBIT", "error", 1]]) do
+      {:error}
+    end
+
+    def command(_, ["GETBIT", "missing", 0]) do
+      {:ok, 0}
+    end
+
+    def command(_, ["GETBIT", "error", 0]) do
+      {:error, "error"}
+    end
+
+    def command(_, ["GETBIT", "success", 0]) do
+      {:ok, 1}
+    end
+
+    def command(_, ["SETBIT", "success", 1, 1]) do
+      {:ok, 1}
+    end
+  end
+
+  test "get_key missing" do
+    assert Ittybitty.Store.get_key("missing", RedisMock) == {:notfound}
+  end
+
+  test "get_key found" do
+    assert Ittybitty.Store.get_key("found", RedisMock) == {:ok, 0}
+  end
+
+  test "get_key error" do
+    assert Ittybitty.Store.get_key("error", RedisMock) == {:error}
+  end
+
+  test "update_key missing" do
+    assert Ittybitty.Store.update_key("missing", 1, RedisMock) == {:notfound}
+  end
+
+  test "update_key success" do
+    assert Ittybitty.Store.update_key("success", 1, RedisMock) == {:ok, 1}
+  end
+
+  test "update_key error" do
+    assert Ittybitty.Store.update_key("error", 1, RedisMock) == {:error, "error"}
+  end
+end
+
