b10649b3cc7b52f50be8f60572daf31593ffd173
Phil Wade <phil@whoisstudio.com>
2017-07-19 16:10:53 -0400

    Pull in realtime bit count

--- ittybitty/web/views/shared_view.ex
+++ ittybitty/web/views/shared_view.ex
@@ -1,3 +1,9 @@
 defmodule Ittybitty.SharedView do
   use Ittybitty.Web, :view
+  alias Ittybitty.Bit
+  alias Ittybitty.Repo
+
+  def bit_count() do
+    Repo.aggregate(Bit, :count, :id)
+  end
 end
